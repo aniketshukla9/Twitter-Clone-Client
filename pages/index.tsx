25f36462c4eea2c21b774967dc48b4ef1edb95a4
Aniket Shukla <student.anishu@gmail.com>
2024-02-19 19:21:42 +0530

    Added Tweet Feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,101 @@
 import Image from "next/image";
-import {BsBell, BsBookmark, BsEnvelope, BsTwitter} from 'react-icons/bs'
-import { BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
-import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import { BiHash, BiHomeCircle, BiImageAlt, BiUser } from "react-icons/bi";
+import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
 import FeedCard from "@/components/FeedCard";
 import { RiTwitterXFill } from "react-icons/ri";
 import { CgMoreO } from "react-icons/cg";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import toast from "react-hot-toast";
 import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
 import { graphqlClient } from "@/clients/api";
 import { useCurrentUser } from "@/hooks/user";
 import { useQueryClient } from "@tanstack/react-query";
+import { useCreateTweet, useGetAllTweets } from "@/hooks/tweet";
+import { Tweet } from "@/gql/graphql";
 
-
-interface TwitterSidebarButton{
-  title: string
-  icon: React.ReactNode
+interface TwitterSidebarButton {
+  title: string;
+  icon: React.ReactNode;
 }
 
 const sidebarMenuItems: TwitterSidebarButton[] = [
   {
-    title : 'Home',
-    icon : <BiHomeCircle /> 
+    title: "Home",
+    icon: <BiHomeCircle />,
   },
   {
-    title : 'Explore',
-    icon : <BiHash />
+    title: "Explore",
+    icon: <BiHash />,
   },
   {
-    title : 'Notifications',
-    icon : <BsBell />
+    title: "Notifications",
+    icon: <BsBell />,
   },
   {
-    title : 'Messages',
-    icon : <BsEnvelope />
+    title: "Messages",
+    icon: <BsEnvelope />,
   },
   {
-    title : 'Bookmarks',
-    icon : <BsBookmark />
+    title: "Bookmarks",
+    icon: <BsBookmark />,
   },
   {
-    title : 'Profile',
-    icon : <BiUser />
+    title: "Profile",
+    icon: <BiUser />,
   },
   {
-    title : 'Premium',
-    icon : <RiTwitterXFill  />
+    title: "Premium",
+    icon: <RiTwitterXFill />,
   },
   {
-    title : 'More',
-    icon : <CgMoreO />
+    title: "More",
+    icon: <CgMoreO />,
   },
 ];
 
 export default function Home() {
-  const {user} = useCurrentUser()
-  const queryClient = useQueryClient()
+  const { user } = useCurrentUser();
+  const { tweets = [] } = useGetAllTweets();
+  const { mutate } = useCreateTweet();
+
+  const queryClient = useQueryClient();
+
+  const [content, setContent] = useState("");
+
+  const handleSelectImage = useCallback(() => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+  }, []);
+
+  const handleCreateTweet = useCallback(() => {
+    mutate({
+      content,
+    });
+  }, [content, mutate]);
 
   const handleLoginWithGoogle = useCallback(
-    async(cred: CredentialResponse) =>{
+    async (cred: CredentialResponse) => {
       const googleToken = cred.credential;
-      if(!googleToken) return toast.error('Google token not found');
+      if (!googleToken) return toast.error("Google token not found");
 
       const { verifyGoogleToken } = await graphqlClient.request(
-        verifyUserGoogleTokenQuery, 
+        verifyUserGoogleTokenQuery,
         { token: googleToken }
-        );
-
+      );
 
       toast.success("Verified Success");
       console.log(verifyGoogleToken);
-  
+
       if (verifyGoogleToken)
-        window.localStorage.setItem('__twitter__token', verifyGoogleToken);
-       
-        await queryClient.invalidateQueries({ queryKey: ['current-user'] });
-      },
-      [queryClient]
-      );
+        window.localStorage.setItem("__twitter__token", verifyGoogleToken);
+
+      await queryClient.invalidateQueries({ queryKey: ["current-user"] });
+    },
+    [queryClient]
+  );
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen pxx-56">
@@ -87,7 +105,7 @@ export default function Home() {
           </div>
           <div className="mt-2 text-xl pr-4">
             <ul>
-              {sidebarMenuItems.map((item) => ( 
+              {sidebarMenuItems.map((item) => (
                 <li
                   className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-3 py-3 w-fit cursor-pointer mt-2"
                   key={item.title}
@@ -99,48 +117,81 @@ export default function Home() {
             </ul>
             <div className="mt-5 px-3">
               <button className="bg-[#1d9bf0] font-semibold text-lg py-2 px-4 rounded-full w-full">
-              Tweet
+                Tweet
               </button>
             </div>
           </div>
           {user && (
-          <div className="absolute bottom-5 flex gap-2 items-center bg-slate-800 px-3 py-2 rounded-full">
-            {user && user.profileImageURL && (
-            <Image
-            className="rounded-full"
-             src={user?.profileImageURL} 
-             alt="user-image" 
-             height={50} 
-             width={50}
-            />
-            )}
-            <div>
-              <h3 className="text-xl">{user.firstName}</h3>
-              <h3 className="text-xl">{user.lastName}</h3>
+            <div className="absolute bottom-5 flex gap-2 items-center bg-slate-800 px-3 py-2 rounded-full">
+              {user && user.profileImageURL && (
+                <Image
+                  className="rounded-full"
+                  src={user?.profileImageURL}
+                  alt="user-image"
+                  height={50}
+                  width={50}
+                />
+              )}
+              <div>
+                <h3 className="text-xl">
+                  {user.firstName} {user.lastName}
+                </h3>
+              </div>
             </div>
-          </div>
           )}
         </div>
         <div className="col-span-5 border-r-[1px] border-l-[1px] hover-screen overflow-scroll border-gray-600">
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
-          <FeedCard />
+          <div>
+            <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all">
+              <div className="grid grid-cols-12 gap-3">
+                <div className="col-span-1">
+                  {user?.profileImageURL && (
+                    <Image
+                      className="rounded-full"
+                      src={user?.profileImageURL}
+                      alt="user-image"
+                      height={50}
+                      width={50}
+                    />
+                  )}
+                </div>
+                <div className="col-span-11">
+                  <textarea
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    className="w-full bg-transparent text-xl px-3 border-b border-slate-700"
+                    placeholder="What's Happening?"
+                    rows={3}
+                  ></textarea>
+                  <div className="mt-5 flex justify-between items-center">
+                    <BiImageAlt
+                      onClick={handleSelectImage}
+                      className="text-xl"
+                    />
+                    <button
+                      onClick={handleCreateTweet}
+                      className="bg-[#1d9bf0] font-semibold text-sm py-2 px-4 rounded-full"
+                    >
+                      Tweet
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {tweets?.map((tweet) =>
+            tweet ? <FeedCard key={tweet?.id} data={tweet as Tweet} /> : null
+          )}
         </div>
         <div className="col-span-3 p-5">
-          {!user && (<div className="p-5 bg-slate-700 rounded-lg">
-            <h1 className="my-2 text-2xl">New to Twitter?</h1>
-            <GoogleLogin onSuccess={handleLoginWithGoogle}/>
-          </div>
-          )}  
+          {!user && (
+            <div className="p-5 bg-slate-700 rounded-lg">
+              <h1 className="my-2 text-2xl">New to Twitter?</h1>
+              <GoogleLogin onSuccess={handleLoginWithGoogle} />
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
-};
+}
