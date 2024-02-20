4302eb6db751164433ec68fed24625ed53718305
Aniket Shukla <student.anishu@gmail.com>
2024-02-20 20:25:00 +0530

    Completed the Profile Page

--- graphql/query/user.ts
+++ graphql/query/user.ts
@@ -3,11 +3,11 @@ import { graphql } from "../../gql";
 export const verifyUserGoogleTokenQuery = graphql(`
   #graphql
   query VerifyUserGoogleToken($token: String!) {
-      verifyGoogleToken(token: $token)
-}   
+    verifyGoogleToken(token: $token)
+  }
 `);
 
-export const getCurrentUserQuery =  graphql(`
+export const getCurrentUserQuery = graphql(`
   query GetCurrentUser {
     getCurrentUser {
       email
@@ -15,6 +15,38 @@ export const getCurrentUserQuery =  graphql(`
       id
       lastName
       profileImageURL
+      tweets {
+        content
+        id
+        author {
+          id
+          firstName
+          lastName
+          profileImageURL
+        }
+      }
+    }
+  }
+`);
+export const getUserByIdQuery = graphql(`
+  #graphql
+
+  query GetuserById($id: ID!) {
+    getUserById(id: $id) {
+      id
+      firstName
+      lastName
+      profileImageURL
+      tweets {
+        content
+        id
+        author {
+          id
+          firstName
+          lastName
+          profileImageURL
+        }
+      }
     }
   }
 `);
