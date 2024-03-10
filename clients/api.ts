cc85155aa6173c47012948301b490a31db58694c
Aniket Shukla <student.anishu@gmail.com>
2024-03-10 14:54:48 +0530

    changes to API URL

--- clients/api.ts
+++ clients/api.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
 
 export const graphqlClient = new GraphQLClient(
-  "https://d35fjovtshhfy.cloudfront.net/graphql",
+  process.env.NEXT_PUBLIC_API_URL as string,
   {
     headers: () => ({
       Authorization: isClient
